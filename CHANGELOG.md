3dc30cdc5197ad08db01708c4018f09dabb2449f
Hannele Valtanen <hannele.valtanen@iki.fi>
2022-01-20 14:53:20 +0200

    Update packages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,16 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.0.3-alpha.0] - Jan 19, 2022
+
+### Components
+#### Added
+- Added component: Alert (draft)
+
+### Tokens
+#### Added
+- Added component specific tokens for Alert
+
 ## [0.0.0] - Jan 19, 2022
 
 ### Added
--- packages/react/src/components/stencil-generated/index.ts
+++ packages/react/src/components/stencil-generated/index.ts
@@ -8,4 +8,5 @@ import type { JSX } from 'nnds-components';
 import { defineCustomElements } from 'nnds-components/loader';
 
 defineCustomElements();
+export const NndsAlert = /*@__PURE__*/createReactComponent<JSX.NndsAlert, HTMLNndsAlertElement>('nnds-alert');
 export const NndsButton = /*@__PURE__*/createReactComponent<JSX.NndsButton, HTMLNndsButtonElement>('nnds-button');
